b9f012bb6888a564cbcd511e3b270ce616af7953
xIssa11 <issahashim@yahoo.com>
2024-03-20 03:17:13 -0700

    Static Part Finished

--- src/App.js
+++ src/App.js
@@ -24,7 +24,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill></FormSplitBill>
     </div>
   );
 }
@@ -60,7 +63,7 @@ function Friend({ friend }) {
 
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
   );
 }
@@ -73,6 +76,37 @@ function FormAddFriend() {
 
       <label>🖼 Image url</label>
       <input type="text" />
+      <Button>Add</Button>
     </form>
   );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2> Split a bill with X</h2>
+
+      <label>💲 Bill Value</label>
+      <input type="text" />
+
+      <label> 🧍‍♂️ Your expense </label>
+      <input type="text" />
+
+      <label> 👬 X's expense </label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill</label>
+
+      <select>
+        <option value="user">You</option>
+        <option value="Friend">Friend</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
 }
